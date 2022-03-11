37ad8f7a6af3a9c11e7139c0b61707b7fd9799f4
Eric Erhardt <eric.erhardt@microsoft.com>
2022-03-11 17:01:00 -0600

    Initial commit of benchmark

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Microsoft.Maui.Graphics.Benchmarks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+		}
+
+
+
+	}
+}
